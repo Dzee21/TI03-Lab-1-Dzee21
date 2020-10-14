fb1c191305e747c601d92f78e3b47ec4e1e735c2
Dzee21 <zei.dhe21@gmail.com>
2020-10-14 06:11:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+a = float (input("Masukan bilangan Pertama "))
+b = float (input("Masukan bilangan Kedua "))
+c = float (input("Masukan bilangan Ketiga "))
+bilangan = (a + b + c)/3  
+print("Rata - rata bilangan",a,b, "dan",c, "adalah",bilangan)
+
+# SOAL 2 - Menulis kelipatan bilangan
+# Tuliskan program untuk Soal 2 di bawah ini
+a = float (input("Masukan sebuah bilangan bulat "))
+bb = a
+bb1 = a**2
+bb2 = a**3
+bb3 = a**4
+bb4 = a**5
+print(bb,"---",bb1, "---",bb2, "---",bb3, "---",bb4)
